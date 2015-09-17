20c9bd4f6dfb71794bfd6a0cf3016f94fb26bd26
ukayare <kohei.tokihara@90202273n.intra.emercury.co.jp>
2015-09-17 23:02:30 +0900

    Fix versioning and remove unused mothod

--- lib/activerecord_json_loader.rb
+++ lib/activerecord_json_loader.rb
@@ -3,6 +3,7 @@ require "activerecord_json_loader/version"
 
 module ActiverecordJsonLoader
   extend ActiveSupport::Concern
+
   module ClassMethods
     def import_from_json(filename)
       json_data = self.load_json filename
@@ -18,65 +19,81 @@ module ActiverecordJsonLoader
       another_attributes.select { |key, value| relation_keys.include? key }
     end
 
+    def get_latest_version
+      # note
+      # This method is published full scan query.
+      # If you think you undesirable this, this method should be overridden to get the latest version in a different way.
+      # ex)
+      # * using cache store latest version.
+      # * using model for version information.
+      if self.attribute_names.include? "version"
+        self.maximum("version").to_i
+      else
+        0
+      end
+    end
+
     protected
     def load_json(filename)
       json_data = open(filename) { |io| JSON.load io }
     end
 
     def import_data(json_data)
+      latest_version = self.get_latest_version
       case json_data
       when Array
         json_data.each do |row_data|
-          self.import_row_data row_data
+          self.import_row_data row_data, latest_version
         end
       when Hash
-        self.import_row_data json_data
+        self.import_row_data json_data, latest_version
       end
     end
 
-    def import_row_data(row_data, klass=self)
+    def import_row_data(row_data, latest_version)
       record_instance = if row_data["id"]
-                          klass.where(id: row_data["id"]).first_or_initialize
+                          self.where(id: row_data["id"]).first_or_initialize
                         else
-                          klass.new
+                          self.new
                         end
-      record_instance.update_row_data row_data
+      record_instance.update_row_data row_data, latest_version
     end
   end
 
-  def update_row_data(row_data)
+  def update_row_data(row_data, latest_version)
     origin_updated = false
     relation_updated = false
     self_attributes, another_attributes = self.class.divide_attributes row_data
-    origin_updated = self.update_origin self_attributes
+    origin_updated = self.update_origin self_attributes, latest_version
     return origin_updated if another_attributes.blank?
     relation_updated = self.update_relation another_attributes
     if (!origin_updated && relation_updated)
-      self.update_with_version
+      self.update_with_version latest_version
     end
     relation_updated
   end
 
-  def update_origin(self_attributes)
+  def update_origin(self_attributes, latest_version)
     self_attributes
     self.attributes = self_attributes
     return false unless self.changed?
-    self.update_with_version
+    self.update_with_version latest_version
     true
   end
 
   def update_relation(another_attributes)
-     self.class.divide_relation_key(another_attributes).select do |key, value|
-      case value
-      when Hash
-        relation_instance = self.try(key) || self.try("build_#{key}")
-        relation_instance.update_row_data value
-      when Array
-        relation_instances = self.try(key).to_a
+    latest_version = self.class.get_latest_version
+    self.class.divide_relation_key(another_attributes).select do |key, value|
+    case value
+    when Hash
+      relation_instance = self.try(key) || self.try("build_#{key}")
+      relation_instance.update_row_data value, latest_version
+    when Array
+      relation_instances = self.try(key).to_a
         updated_flag = false
         value.each_with_index do |relation_attributes, i|
           relation_instance = relation_instances[i] || self.try(key).build
-          relation_updated = relation_instance.update_row_data relation_attributes
+          relation_updated = relation_instance.update_row_data relation_attributes, latest_version
           updated_flag ||= relation_updated
         end
         relation_deleted = self.delete_remain_relation(relation_instances[value.size..-1])
@@ -88,9 +105,9 @@ module ActiverecordJsonLoader
   end
 
 
-  def update_with_version
+  def update_with_version(latest_version)
     if self.respond_to?(:version)
-      self.version = self.version.to_i + 1
+      self.version = latest_version + 1
     end
     if self.class.respond_to? :with_writable
       self.class.with_writable { self.save }
@@ -109,26 +126,5 @@ module ActiverecordJsonLoader
       end
     end
     true
-  end
-
-  def update_relation_instance(relation_name, value)
-    relation_instance = self.try(relation_name) || self.try("build_#{relation_name}")
-    return false if relation_instance.blank?
-    relation_instance.attributes = value
-    return false unless relation_instance.changed?
-    relation_instance.update_with_version
-  end
-
-  def update_relation_instances(relation_name, values)
-    relation_instances = self.try(relation_name).all
-    updated_flag = false
-    values.each_with_index do |relation_attributes, i|
-      relation_instance = relation_instances[i] || self.try(relation_name).build
-      relation_instance.attributes = relation_attributes
-      next unless relation_instance.changed?
-      relation_instance.update_with_version
-      updated_flag = true
-    end
-    updated_flag || self.delete_remain_relation(relation_instances[values.size..-1])
   end
 end
